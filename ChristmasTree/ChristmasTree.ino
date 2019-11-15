f6bc995fb7be6307315a32a97048970007946ead
Mark Ellwood <mark@the-ellwoods.org>
2019-11-15 02:41:45 +0000

    Added comment

--- ChristmasTree/ChristmasTree.ino
+++ ChristmasTree/ChristmasTree.ino
@@ -1,3 +1,5 @@
+// This is needed as the ESP fires interrups that can take longer than 50us to process and so
+// disrupt the sending of data to the lights 
 #define FASTLED_ALLOW_INTERRUPTS 0
 
 #include <FastLED.h>
