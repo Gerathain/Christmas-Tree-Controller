b131c1b7445d284551855303f67c2ed13738ce0e
Mark Ellwood <mark@the-ellwoods.org>
2019-12-09 22:54:43 +0000

    Now send a response to the command sender

--- ChristmasTree/parser.cpp
+++ ChristmasTree/parser.cpp
@@ -1,8 +1,16 @@
 #include "parser.h"
 
+String s = "Command received";
+
 void checkNewMode(int* pattern, ESP8266WebServer* server)
 {
+  Serial.println("Pattern changing");
   String newPattern = server->arg("pattern"); // if the pattern arg isn't present then "" is returned
+  server->sendHeader("Connection", "close");
+  server->send(200, "text/html", s);
+  
+  Serial.print("new pattern: ");
+  Serial.println(newPattern);
   *pattern = newPattern.toInt(); // This really needs range checking
   return;
 }
