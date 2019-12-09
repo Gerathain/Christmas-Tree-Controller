4e9f1c703e3b861c4bd21ab2c5c0ed0deba3aff7
Mark Ellwood <mark@the-ellwoods.org>
2019-12-09 22:20:27 +0000

    Added code for new server that accepts uploaded files and handles incoming get requests (non-working)

--- ChristmasTree/ChristmasTree.ino
+++ ChristmasTree/ChristmasTree.ino
@@ -1,5 +1,9 @@
 #include <ESP8266WiFi.h>
 
+#include <WiFiClient.h>
+#include <ESP8266WebServer.h>
+#include <ESP8266mDNS.h>
+
 #include "WifiSecret.h"
 //This has the defines for the wifi ssid and password
 
@@ -15,12 +19,19 @@
 
 // Create an instance of the server
 // specify the port to listen on as an argument
-WiFiServer server(80);
+ESP8266WebServer server(80);
+const char* serverIndex = "<form method='POST' action='/update' enctype='multipart/form-data'><input type='file' name='update'><input type='submit' value='Update'></form>";
+const char* host = "esp8266-webupdate";
 
 // This is the array of all of the possible patterns
 void (* patterns[])(CRGB*) = {rainbow, warmGlitter};
 CRGB leds[NUM_LEDS];
 int pattern = 0;
+
+void parseConnection()
+{
+  return;
+}
 
 void setupWifi()
 {
@@ -39,7 +50,49 @@ void setupWifi()
   Serial.println("");
   Serial.println("WiFi connected");
 
+  MDNS.begin(host);
+  
+  // Handle the default connection for people to upload
+  server.on("/", HTTP_GET, []() {
+    server.sendHeader("Connection", "close");
+    server.send(200, "text/html", serverIndex);
+  });
+
+  // Handle people connecting to change the pattern
+  server.on("/pattern", HTTP_GET, parseConnection);
+
+  // Handle the posting of updates
+  server.on("/update", HTTP_POST, []() {
+    server.sendHeader("Connection", "close");
+    server.send(200, "text/plain", (Update.hasError()) ? "FAIL" : "OK");
+    ESP.restart();
+  }, []() {
+    HTTPUpload& upload = server.upload();
+    if (upload.status == UPLOAD_FILE_START) {
+      Serial.setDebugOutput(true);
+      WiFiUDP::stopAll();
+      Serial.printf("Update: %s\n", upload.filename.c_str());
+      uint32_t maxSketchSpace = (ESP.getFreeSketchSpace() - 0x1000) & 0xFFFFF000;
+      if (!Update.begin(maxSketchSpace)) { //start with max available size
+        Update.printError(Serial);
+      }
+    } else if (upload.status == UPLOAD_FILE_WRITE) {
+      if (Update.write(upload.buf, upload.currentSize) != upload.currentSize) {
+        Update.printError(Serial);
+      }
+    } else if (upload.status == UPLOAD_FILE_END) {
+      if (Update.end(true)) { //true to set the size to the current progress
+        Serial.printf("Update Success: %u\nRebooting...\n", upload.totalSize);
+      } else {
+        Update.printError(Serial);
+      }
+      Serial.setDebugOutput(false);
+    }
+    yield();
+  });
   server.begin();
+  MDNS.addService("http", "tcp", 80);
+  
   Serial.println("Server started");
 
   // Print the IP address
