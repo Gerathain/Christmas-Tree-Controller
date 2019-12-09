15c4a49faa7b237a11c7c72a2dd614db11cc31fe
Mark Ellwood <mark@the-ellwoods.org>
2019-12-09 22:09:52 +0000

    Removed code for OTA updating as the port is super flakey

--- ChristmasTree/ChristmasTree.ino
+++ ChristmasTree/ChristmasTree.ino
@@ -1,7 +1,4 @@
 #include <ESP8266WiFi.h>
-#include <ESP8266mDNS.h>
-#include <WiFiUdp.h>
-#include <ArduinoOTA.h>
 
 #include "WifiSecret.h"
 //This has the defines for the wifi ssid and password
@@ -70,49 +67,12 @@ void setup()
   // initialise the pin for turning the PSU on/off
   pinMode(POWER_PIN, OUTPUT);
   digitalWrite(POWER_PIN, LOW);
-
-  // Code for OTA updates
-  ArduinoOTA.onStart([]() {
-    String type;
-    if (ArduinoOTA.getCommand() == U_FLASH) {
-      type = "sketch";
-    } else { // U_FS
-      type = "filesystem";
-    }
-
-    // NOTE: if updating FS this would be the place to unmount FS using FS.end()
-    Serial.println("Start updating " + type);
-  });
-  ArduinoOTA.onEnd([]() {
-    Serial.println("\nEnd");
-  });
-  ArduinoOTA.onProgress([](unsigned int progress, unsigned int total) {
-    Serial.printf("Progress: %u%%\r", (progress / (total / 100)));
-  });
-  ArduinoOTA.onError([](ota_error_t error) {
-    Serial.printf("Error[%u]: ", error);
-    if (error == OTA_AUTH_ERROR) {
-      Serial.println("Auth Failed");
-    } else if (error == OTA_BEGIN_ERROR) {
-      Serial.println("Begin Failed");
-    } else if (error == OTA_CONNECT_ERROR) {
-      Serial.println("Connect Failed");
-    } else if (error == OTA_RECEIVE_ERROR) {
-      Serial.println("Receive Failed");
-    } else if (error == OTA_END_ERROR) {
-      Serial.println("End Failed");
-    }
-  });
-  ArduinoOTA.begin();
-  // End code for OTA updates
   
   Serial.println("Ready");
 }
 
 void loop()
-{
-  ArduinoOTA.handle();
-  
+{  
   checkNewMode(&pattern, &server); // This is just checking if someone is trying to connect to the server
 
   patterns[pattern](leds);
